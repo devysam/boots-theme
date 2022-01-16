01cf1b9a0928cbc060a7c4c64de43a77a1a96615
shummy yadav <yadav.shummy1234@gmail.com>
2022-01-16 16:37:41 +0530

    slider created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HOME PAGE</h1>
+      <input />
+      <ol>
+        <li>HOME</li>
+        <li>PAGE</li>
+      </ol>
+    </div>
+  );
+}
+export default App;
